e5123f38797f2a4c483b9145ea2801d7b34e5ab1
lohithram <l.ram@affectivelogic.com>
2015-09-29 23:38:25 +0100

    Update unit test

--- __tests__/BillList.js
+++ __tests__/BillList.js
@@ -5,12 +5,28 @@ var React = require('react/addons');
 	var TestUtils = React.addons.TestUtils;
 	var BillList = require('../jsx/BillList.jsx');
 
+
 	var billListComponent;
 	
 	beforeEach(function(){
 
+		var callItemRenderer = require('../jsx/CallsBillItem.jsx');
+
 		billListComponent = TestUtils.renderIntoDocument(
-				<BillList/>);
+				<BillList sectionTitle={'testTitle'}
+							headerTitles={['header1', 'header2']}
+							billedItems={[]}
+							itemRenderer={callItemRenderer}/>);
 
 	});
+
+
+	it('Check all the properties are declared upfront on the component', function() {
+
+		expect(BillList.propTypes.sectionTitle).not.toBeUndefined();
+		expect(BillList.propTypes.billedItems).not.toBeUndefined();
+		expect(BillList.propTypes.headerTitles).not.toBeUndefined();
+		expect(BillList.propTypes.itemRenderer).not.toBeUndefined();			
+	});
+
 });
